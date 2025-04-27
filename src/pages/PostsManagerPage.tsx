7dcbee0286b2cee6de4ae724fa3e8fd338545a5a
ssp <qkrtjdtlr02@naver.com>
2025-04-27 14:48:03 +0900

    shared/ui Button 분리

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react"
 import { Edit2, MessageSquare, Plus, Search, ThumbsDown, ThumbsUp, Trash2 } from "lucide-react"
 import { useLocation, useNavigate } from "react-router-dom"
 import {
-  Button,
   Card,
   CardContent,
   CardHeader,
@@ -25,6 +24,7 @@ import {
   TableRow,
   Textarea,
 } from "../shared/ui"
+import Button from "../shared/ui/button"
 
 const PostsManager = () => {
   const navigate = useNavigate()
@@ -268,7 +268,6 @@ const PostsManager = () => {
   // 댓글 좋아요
   const likeComment = async (id, postId) => {
     try {
-
       const response = await fetch(`/api/comments/${id}`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
@@ -277,7 +276,9 @@ const PostsManager = () => {
       const data = await response.json()
       setComments((prev) => ({
         ...prev,
-        [postId]: prev[postId].map((comment) => (comment.id === data.id ? {...data, likes: comment.likes + 1} : comment)),
+        [postId]: prev[postId].map((comment) =>
+          comment.id === data.id ? { ...data, likes: comment.likes + 1 } : comment,
+        ),
       }))
     } catch (error) {
       console.error("댓글 좋아요 오류:", error)
