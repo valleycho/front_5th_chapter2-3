8ef5dfff98dca3f1ef68bf347f4d6d751f191c7f
ssp <qkrtjdtlr02@naver.com>
2025-04-27 15:16:03 +0900

    ♻️ : shared/ui Select 분리

---
+++ src/shared/ui/select/Select.tsx
@@ -0,0 +1,5 @@
+import * as SelectPrimitive from "@radix-ui/react-select"
+
+export const Select = SelectPrimitive.Root
+export const SelectGroup = SelectPrimitive.Group
+export const SelectValue = SelectPrimitive.Value
---
+++ src/shared/ui/select/index.ts
@@ -0,0 +1,4 @@
+export { Select, SelectGroup, SelectValue } from "./Select"
+export { SelectTrigger } from "./SelectTrigger"
+export { SelectContent } from "./SelectContent"
+export { SelectItem } from "./SelectItem"
