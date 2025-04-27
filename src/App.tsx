92563897ef5c7b447144ef5d6cced6c2ccd57f82
ssp <qkrtjdtlr02@naver.com>
2025-04-27 19:08:17 +0900

    ♻️ : 직관적이게 widgets, pages의 파일 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import Header from "./widgets/layout/header/ui/Header.tsx"
+import Footer from "./widgets/layout/footer/ui/Footer.tsx"
+import PostsManagerPage from "./pages/posts/ui/PostsManagerPage.tsx"
 
 const App = () => {
   return (
