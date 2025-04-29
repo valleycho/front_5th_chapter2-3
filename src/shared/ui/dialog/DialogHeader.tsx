ae1812b89e05fe1550b40b26ba3e05e29efcfeae
ssp <qkrtjdtlr02@naver.com>
2025-04-29 15:37:50 +0900

    🏷️ : dialog 관련 타입 지정

--- src/shared/ui/dialog/DialogHeader.tsx
+++ src/shared/ui/dialog/DialogHeader.tsx
@@ -1,4 +1,10 @@
-export const DialogHeader = ({ className, ...props }) => (
+import * as DialogPrimitive from "@radix-ui/react-dialog"
+
+interface DialogHeaderProps extends DialogPrimitive.DialogTitleProps {
+  className?: string
+}
+
+export const DialogHeader = ({ className, ...props }: DialogHeaderProps) => (
   <div className={`flex flex-col space-y-1.5 text-center sm:text-left ${className}`} {...props} />
 )
 DialogHeader.displayName = "DialogHeader"
--- src/shared/ui/dialog/DialogTitle.tsx
+++ src/shared/ui/dialog/DialogTitle.tsx
@@ -1,7 +1,11 @@
 import { forwardRef } from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 
-export const DialogTitle = forwardRef(({ className, ...props }, ref) => (
+interface DialogTitleProps extends DialogPrimitive.DialogTitleProps {
+  className?: string
+}
+
+export const DialogTitle = forwardRef<HTMLHeadingElement, DialogTitleProps>(({ className, ...props }, ref) => (
   <DialogPrimitive.Title
     ref={ref}
     className={`text-lg font-semibold leading-none tracking-tight ${className}`}
