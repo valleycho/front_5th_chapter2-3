322779a5b994a0de5c9b5a11e8a989f995b55c99
ssp <qkrtjdtlr02@naver.com>
2025-05-01 00:42:14 +0900

    🔧: 배포 환경설정 변경 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import { resolve } from "path"
+import path from "path"
+
+const isCI = process.env.CI === "true";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/front_5th_chapter2-3/",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
+  base: isCI ? "" : "/front_5th_chapter2-3/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
